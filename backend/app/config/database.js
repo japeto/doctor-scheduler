d1e362911a5be206c69acf6a1bbbcf2278d2adc7
jeffersonamado <jeffersonamado@gmail.com>
2024-01-27 08:48:52 -0500

    refactor: improve conectar method.

--- backend/app/config/database.js
+++ backend/app/config/database.js
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
 
-async function conectar(){
+ const conectar = async()=> {
   // template
   // mongoose.connect("mongodb://username:password@127.0.0.1:27017/dscheduler");
-  mongoose.connect(`mongodb://${process.env.DBHOST}:${process.env.DBPORT}/${process.env.DBNAME}`)
-  return mongoose.connection;
+  try{
+    const dconnection = await mongoose.connect(`mongodb://${process.env.DBHOST}:${process.env.DBPORT}/${process.env.DBNAME}`)
+    
+    dconnection.on("error", function(err){
+      console.log(`Connection error,  ${err}`);
+    });
+
+    dconnection.on("open", ()=>{
+      console.log("Successful connection");
+    });
+
+    return  mongoose.connection;
+  }catch(err){
+
+  }
 }
 
 export default conectar;
